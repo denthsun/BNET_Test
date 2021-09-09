776b17b3769dddaa3f35581470ad3f5c6edf4655
denVelikanov <denthesun@gmail.com>
2021-09-09 13:43:46 +0300

    model update

--- BNET_Test/Models/EntriesListModel.swift
+++ BNET_Test/Models/EntriesListModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct EntriesListModel: Decodable {
+    var status: Int
+    var data: [[EntriesListDataModel]]
+}
+
+struct EntriesListDataModel: Decodable {
+    var id: String
+    var body: String
+    var da: String
+    var dm: String
+}
+
+
---
+++ Pods/Target Support Files/Pods-BNET_Test/Pods-BNET_Test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BNET_Test : NSObject
+@end
+@implementation PodsDummy_Pods_BNET_Test
+@end
